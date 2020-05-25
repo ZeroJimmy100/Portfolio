15a77e89cd9658b4b77c96f39c94a9bf20131cc3
ZeroJimmy100 <61480298+ZeroJimmy100@users.noreply.github.com>
2020-05-24 20:10:00 -0700

    Update service-worker.js

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dd1a19d962d6aea2c86f509bfcf9b3e0.js"
+  "/precache-manifest.201b9dc22559287e0298e1c793fecc51.js"
 );
 
 self.addEventListener('message', (event) => {
