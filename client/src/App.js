f23a7a5a0552e2148fe3ed4b68adc7d3e38e03ba
ZeroJimmy100 <61480298+ZeroJimmy100@users.noreply.github.com>
2020-05-24 19:00:31 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import logo from './logo.svg'; // import the logo of react
 import './App.css';
 import { Router } from '@reach/router';
+
 import Main from './views/Main';
 
 function App() {
