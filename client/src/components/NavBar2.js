a1c4203a366dfee91d18b7c36a32c49680162c58
Jimmy Pham <jimmyp.dev3@gmail.com>
2020-06-05 19:26:43 -0700

    Update NavBar2.js

--- client/src/components/NavBar2.js
+++ client/src/components/NavBar2.js
@@ -13,7 +13,7 @@ export default function NavBar2() {
                     <li><a href="#SectionTwo">About me</a></li>
                     <li><a href="#SectionThree">Projects</a></li>
                     <li><a href="#SectionFour">Testimonials</a></li>
-                    <li><a href="https://drive.google.com/file/d/1uf_GEdfXLrhZ6jXmRfL_zt77mG7_Mkm7/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a></li>
+                    <li><a href="https://drive.google.com/file/d/1wqse8iFLxoRR7jxekQRO3K7tzCnZ0z5e/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a></li>
                     <li><a href="https://github.com/ZeroJimmy100" target="_blank" rel="noopener noreferrer">Github</a></li>
                 </ul>
             </header>
