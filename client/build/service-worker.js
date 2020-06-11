416143624e364ff38cbb8ecfd66fcf9caf66b050
Jimmy Pham <jimmyp.dev3@gmail.com>
2020-06-10 17:54:22 -0700

    Update service-worker.js

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.00b5d84fb6faea88bf31235ebcfe6fee.js"
+  "/precache-manifest.6d6b71a21f4b3d8cbae36e25135fc29e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
